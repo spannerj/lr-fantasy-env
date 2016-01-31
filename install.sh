6d50c5461ca7064dd7d43ebe10b45a2bbf7bc0ed
spannerj <sj130773>
2016-01-31 17:07:01 +0000

    Amended Vagrant file and install script to ensure correct folder sharing and to check out code repo.

--- install.sh
+++ install.sh
@@ -3,6 +3,20 @@
 echo "install git"
 sudo yum install git -y
 
+
+echo "create code folder"
+mkdir 'code'
+cd 'code'
+
+echo "add ssh key to known hosts"
+ssh-keyscan -H github.com >> /etc/ssh/ssh_known_hosts
+
+echo
+echo "clone repo"
+if [ ! -d "lrfantasy" ]; then
+    git clone 'git@github.com:spannerj/lrfantasy.git'
+fi
+
 #echo "install postgres libs"
 #sudo yum install postgresql-libs -y
 #
